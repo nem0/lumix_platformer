d88684c727563daae06ea3d2f59a9a95d95f20e3
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-03-27 23:54:53 +0100

    compatible with latest engine

--- pipelines/common.glsl
+++ pipelines/common.glsl
@@ -27,7 +27,7 @@ const vec2 POISSON_DISK_16[16] = vec2[](
 	vec2(0.6104985,0.7838438)
 );
 
-struct Probe {
+struct EnvProbe {
 	vec4 pos;
 	vec4 rot;
 	vec4 inner_range;
@@ -43,6 +43,12 @@ struct Probe {
 	vec4 sh_coefs8;
 };
 
+struct ReflProbe {
+	vec4 pos_layer;
+	vec4 rot;
+	vec4 half_extents;
+};
+
 struct Light {
 	vec4 pos_radius;
 	vec4 rot;
@@ -55,7 +61,8 @@ struct Light {
 struct Cluster {
 	int offset;
 	int lights_count;
-	int probes_count;
+	int env_probes_count;
+	int refl_probes_count;
 };
 
 struct Surface {
@@ -64,6 +71,8 @@ struct Surface {
 	float roughness;
 	float metallic;
 	float emission;
+	float translucency;
+	float ao;
 	vec3 N;
 	vec3 V;
 	vec3 wpos;
@@ -107,30 +116,36 @@ layout (std140, binding = 1) uniform PassState {
 	vec4 view_dir;
 	vec4 camera_up;
 	vec4 camera_planes[6];
+	vec4 shadow_to_camera;
 } Pass;
  
 layout (std140, binding = 3) uniform ShadowAtlas {
 	mat4 u_shadow_atlas_matrices[128];
 };
 
-layout(std430, binding = 6) readonly buffer lights
+layout(std430, binding = 7) readonly buffer lights
 {
 	Light b_lights[];
 };
 
-layout(std430, binding = 7) readonly buffer clusters
+layout(std430, binding = 8) readonly buffer clusters
 {
 	Cluster b_clusters[];
 };
 	
-layout(std430, binding = 8) readonly buffer cluster_maps
+layout(std430, binding = 9) readonly buffer cluster_maps
 {
 	int b_cluster_map[];
 };
 
-layout(std430, binding = 9) readonly buffer probes
+layout(std430, binding = 10) readonly buffer envprobes
 {
-	Probe b_probes[];
+	EnvProbe b_env_probes[];
+};
+
+layout(std430, binding = 11) readonly buffer reflprobes
+{
+	ReflProbe b_refl_probes[];
 };
 
 
@@ -356,7 +371,8 @@ vec3 computeDirectLight(Surface surface, vec3 L, vec3 light_color)
 	vec3 H = normalize(surface.V + L);
 	float ldoth = saturate(dot(L, H));
 	float ndoth = saturate(dot(surface.N, H));
-	float ndotl = saturate(dot(surface.N, L));
+	float ndotl_full = dot(surface.N, L);
+	float ndotl = saturate(ndotl_full);
 	float hdotv = saturate(dot(H, surface.V));
 	
 	// D GGX
@@ -378,17 +394,17 @@ vec3 computeDirectLight(Surface surface, vec3 L, vec3 light_color)
 	vec3 kD = vec3(1.0) - F;
 	kD *= 1.0 - surface.metallic;
 	
-	return (kD * surface.albedo / M_PI + specular) * light_color * ndotl;
+	vec3 diffuse = kD * surface.albedo / M_PI;
+	return (diffuse + specular) * light_color * ndotl
+		+ surface.translucency * diffuse * light_color * max(0, -ndotl_full);
 }	
 
-
-vec3 env_brdf_approx (vec3 F0, float roughness, float NoV)
-{
-	vec4 c0 = vec4(-1, -0.0275, -0.572, 0.022 );
-	vec4 c1 = vec4(1, 0.0425, 1.0, -0.04 );
+vec3 env_brdf_approx(vec3 F0, float roughness, float NoV) {
+	vec4 c0 = vec4(-1, -0.0275, -0.572, 0.022);
+	vec4 c1 = vec4(1, 0.0425, 1.0, -0.04);
 	vec4 r = roughness * c0 + c1;
-	float a004 = min( r.x * r.x, exp2( -9.28 * NoV ) ) * r.x + r.y;
-	vec2 AB = vec2( -1.04, 1.04 ) * a004 + r.zw;
+	float a004 = min(r.x * r.x, exp2(-9.28 * NoV)) * r.x + r.y;
+	vec2 AB = vec2(-1.04, 1.04) * a004 + r.zw;
 	return F0 * AB.x + AB.y;
 }
 
@@ -400,18 +416,7 @@ vec3 computeIndirectDiffuse(vec3 irradiance, Surface surface) {
 	return surface.albedo * irradiance;
 }
 
-
-vec3 computeIndirectSpecular(samplerCube radiancemap, Surface surface) {
-	float ndotv = abs(dot(surface.N , surface.V)) + 1e-5f;
-	vec3 F0 = mix(vec3(0.04), surface.albedo, surface.metallic);		
-	float lod = surface.roughness * 5;
-	vec3 RV = reflect(-surface.V, surface.N);
-	vec4 radiance_rgbm = textureLod(radiancemap, RV, lod);
-	vec3 radiance = radiance_rgbm.rgb * radiance_rgbm.a * 4;
-	return radiance * env_brdf_approx(F0, surface.roughness, ndotv);
-}
-
-vec3 evalSH(Probe probe, vec3 N) {
+vec3 evalSH(EnvProbe probe, vec3 N) {
 	return probe.sh_coefs0.rgb
 	+ probe.sh_coefs1.rgb * (N.y)
 	+ probe.sh_coefs2.rgb * (N.z)
@@ -433,17 +438,48 @@ vec3 rotateByQuat(vec4 rot, vec3 pos)
 	return pos + uv + uuv;
 }
 
+vec3 reflProbesLighting(Cluster cluster, Surface surface, samplerCubeArray reflection_probes) {
+	int from = cluster.offset + cluster.lights_count + cluster.env_probes_count;
+	int to = from + cluster.refl_probes_count;
+	float ndotv = abs(dot(surface.N , surface.V)) + 1e-5f;
+	vec3 F0 = mix(vec3(0.04), surface.albedo, surface.metallic);		
+	vec3 brdf = env_brdf_approx(F0, surface.roughness, ndotv);
+	float lod = surface.roughness * 5;
+	vec3 RV = reflect(-surface.V, surface.N);
+	
+	float remaining_w = 1;
+	vec3 res = vec3(0);
+	for (int i = from; i < to; ++i) {
+		int probe_idx = b_cluster_map[i]; 
+		vec4 rot = b_refl_probes[probe_idx].rot;
+		vec3 lpos = b_refl_probes[probe_idx].pos_layer.xyz - surface.wpos;
+		uint layer = floatBitsToUint(b_refl_probes[probe_idx].pos_layer.w);
+		vec4 radiance_rgbm = textureLod(reflection_probes, vec4(RV, layer), lod);
+		vec3 radiance = radiance_rgbm.rgb * radiance_rgbm.a * 4;
+
+		lpos = rotateByQuat(rot, lpos);
+		vec3 half_extents = b_refl_probes[probe_idx].half_extents.xyz;
+		vec3 rpos = saturate(abs(lpos) / half_extents * 2 - 1);
+		float w = 1 - max(rpos.x, max(rpos.y, rpos.z));
+		w = min(remaining_w, w);
+		remaining_w -= w;
+		res += radiance * brdf * w;
+	}
+
+	return (remaining_w > 0.999 ? vec3(0) : res / (1 - remaining_w)) * surface.ao * Global.light_indirect_intensity;
+}
+
 vec3 envProbesLighting(Cluster cluster, Surface surface) {
 	float remaining_w = 1;
 	vec3 probe_light = vec3(0);
 	int from = cluster.offset + cluster.lights_count;
-	int to = from + cluster.probes_count;
+	int to = from + cluster.env_probes_count;
 	for (int i = from; i < to; ++i) {
 		int probe_idx = b_cluster_map[i]; 
-		vec3 lpos = b_probes[probe_idx].pos.xyz - surface.wpos.xyz;
-		vec4 rot = b_probes[probe_idx].rot;
-		vec3 outer_range = b_probes[probe_idx].outer_range.xyz;
-		vec3 inner_range = b_probes[probe_idx].inner_range.xyz;
+		vec3 lpos = b_env_probes[probe_idx].pos.xyz - surface.wpos.xyz;
+		vec4 rot = b_env_probes[probe_idx].rot;
+		vec3 outer_range = b_env_probes[probe_idx].outer_range.xyz;
+		vec3 inner_range = b_env_probes[probe_idx].inner_range.xyz;
 			
 		lpos = rotateByQuat(rot, lpos);
 		lpos = max(abs(lpos) - inner_range, vec3(0));
@@ -456,51 +492,73 @@ vec3 envProbesLighting(Cluster cluster, Surface surface) {
 		w = min(remaining_w, w);
 		remaining_w -= w;
 
-		vec3 irradiance = evalSH(b_probes[probe_idx], surface.N);
+		vec3 irradiance = evalSH(b_env_probes[probe_idx], surface.N);
 		irradiance = max(vec3(0), irradiance);
 		vec3 indirect = computeIndirectDiffuse(irradiance, surface);
 		probe_light += (indirect * Global.light_indirect_intensity) * w / M_PI;
 		if (remaining_w <= 0) break;
 	}
-	return remaining_w < 1 ? probe_light / (1 - remaining_w) : vec3(0);
+	return (remaining_w < 1 ? probe_light / (1 - remaining_w) : vec3(0)) * surface.ao;
+}
+
+// must match ShadowAtlas::getUV
+float getShadowAtlasResolution(int idx) {
+	if (idx == 0) return 1024;
+	if (idx < 5) return 512;
+	return 256;
 }
 
 vec3 pointLightsLighting(Cluster cluster, Surface surface, sampler2D shadow_atlas) {
-	vec3 res = vec3(0);
-	for (int i = cluster.offset; i < cluster.offset + cluster.lights_count; ++i) {
-		int light_idx = b_cluster_map[i]; 
-		vec3 lpos = surface.wpos.xyz - b_lights[light_idx].pos_radius.xyz;
-		float dist = length(lpos);
-		float attn = pow(max(0, 1 - dist / b_lights[light_idx].pos_radius.w), b_lights[light_idx].color_attn.w);
-		vec3 L = -lpos / dist;
-		if (attn > 1e-5) {
-			vec3 direct_light = computeDirectLight(surface, L, b_lights[light_idx].color_attn.rgb);
-			int atlas_idx = b_lights[light_idx].atlas_idx;
-			if (atlas_idx >= 0) {
-				vec4 proj_pos = u_shadow_atlas_matrices[atlas_idx] * vec4(lpos, 1);
-				proj_pos /= proj_pos.w;
+	#ifdef LUMIX_FRAGMENT_SHADER
+		vec3 res = vec3(0);
+		for (int i = cluster.offset; i < cluster.offset + cluster.lights_count; ++i) {
+			int light_idx = b_cluster_map[i]; 
+			vec3 lpos = surface.wpos.xyz - b_lights[light_idx].pos_radius.xyz;
+			float dist = length(lpos);
+			float attn = pow(max(0, 1 - dist / b_lights[light_idx].pos_radius.w), b_lights[light_idx].color_attn.w);
+			vec3 L = -lpos / dist;
+			if (attn > 1e-5) {
+				vec3 direct_light = computeDirectLight(surface, L, b_lights[light_idx].color_attn.rgb);
+				int atlas_idx = b_lights[light_idx].atlas_idx;
+				if (atlas_idx >= 0) {
+					vec4 proj_pos = u_shadow_atlas_matrices[atlas_idx] * vec4(lpos, 1);
+					proj_pos /= proj_pos.w;
 
-				vec2 shadow_uv = proj_pos.xy;
-				float occluder = textureLod(shadow_atlas, shadow_uv, 0).r;
-				float receiver = proj_pos.z;
-				attn *= receiver * 1.01 > occluder ? 1 : 0;
+					vec2 shadow_uv = proj_pos.xy;
+
+					float c = random(vec2(gl_FragCoord)) * 2 - 1;
+					float s = sqrt(1 - c * c); 
+					mat2 rot = mat2(c, s, -s, c);
+					float shadow = 0;
+					float receiver = proj_pos.z;
+					for (int j = 0; j < 16; ++j) {
+						vec2 pcf_offset = POISSON_DISK_16[j] * rot;
+						vec2 uv = shadow_uv + pcf_offset * vec2(0.25, 1) / getShadowAtlasResolution(atlas_idx) * 3;
+
+						float occluder = textureLod(shadow_atlas, uv, 0).r;
+						shadow += receiver * 1.02 > occluder ? 1 : 0;
+					}
+					attn *= shadow / 16;
+				}
+
+				float fov = b_lights[light_idx].fov;
+				if (fov < M_PI) {
+					// TODO replace rot with dir
+					vec3 dir = rotateByQuat(b_lights[light_idx].rot, vec3(0, 0, -1));
+					vec3 L = lpos / max(dist, 1e-5);
+					float cosDir = dot(normalize(dir), L);
+					float cosCone = cos(fov * 0.5);
+
+					attn *= cosDir < cosCone ? 0 : (cosDir - cosCone) / (1 - cosCone);
+				}
+
+				res += direct_light * attn;
 			}
-
-			float fov = b_lights[light_idx].fov;
-			if (fov < M_PI) {
-				// TODO replace rot with dir
-				vec3 dir = rotateByQuat(b_lights[light_idx].rot, vec3(0, 0, -1));
-				vec3 L = lpos / max(dist, 1e-5);
-				float cosDir = dot(normalize(dir), L);
-				float cosCone = cos(fov * 0.5);
-
-				attn *= cosDir < cosCone ? 0 : (cosDir - cosCone) / (1 - cosCone);
-			}
-
-			res += direct_light * attn;
 		}
-	}
-	return res;
+		return res;
+	#else
+		return vec3(0);
+	#endif
 }
 
 float rand(vec3 seed)
@@ -509,16 +567,16 @@ float rand(vec3 seed)
 	return fract(sin(dot_product) * 43758.5453);
 }
 
-vec3 vegetationAnim(vec3 obj_pos, vec3 vertex_pos) {
+vec3 vegetationAnim(vec3 obj_pos, vec3 vertex_pos, float strength) {
 	obj_pos += Global.camera_world_pos.xyz;
-	vertex_pos.x += vertex_pos.y > 0.1 ? cos((obj_pos.x + obj_pos.y + obj_pos.z * 2) * 0.3 + Global.time * 2) * vertex_pos.y * vertex_pos.y * 0.001 : 0;
+	vertex_pos.x += vertex_pos.y > 0.1 ? cos((obj_pos.x + obj_pos.y + obj_pos.z * 2) * 0.3 + Global.time * 2) * vertex_pos.y * vertex_pos.y * strength : 0;
 	return vertex_pos;
 }
 
 void packSurface(Surface surface, out vec4 gbuffer0, out vec4 gbuffer1, out vec4 gbuffer2) {
 	gbuffer0 = vec4(surface.albedo.rgb, surface.roughness);
 	gbuffer1 = vec4(surface.N * 0.5 + 0.5, surface.metallic);
-	gbuffer2 = vec4(packEmission(surface.emission), 0, 0, 1);
+	gbuffer2 = vec4(packEmission(surface.emission), surface.translucency, surface.ao, 1);
 }
 
 Surface unpackSurface(vec2 uv, sampler2D gbuffer0, sampler2D gbuffer1, sampler2D gbuffer2, sampler2D gbuffer_depth, out float ndc_depth) {
@@ -534,10 +592,12 @@ Surface unpackSurface(vec2 uv, sampler2D gbuffer0, sampler2D gbuffer1, sampler2D
 	surface.emission = unpackEmission(gb2.x);
 	surface.wpos = getViewPosition(gbuffer_depth, Global.inv_view_projection, uv, ndc_depth);
 	surface.V = normalize(-surface.wpos);
+	surface.translucency = gb2.y;
+	surface.ao = gb2.z;
 	return surface;
 }
 
-vec3 computeLighting(Cluster cluster, Surface surface, vec3 light_direction, vec3 light, sampler2D shadowmap, sampler2D shadow_atlas) {
+vec3 computeLighting(Cluster cluster, Surface surface, vec3 light_direction, vec3 light, sampler2D shadowmap, sampler2D shadow_atlas, samplerCubeArray reflection_probes) {
 	float shadow = getShadow(shadowmap, surface.wpos, surface.N);
 	vec3 res = computeDirectLight(surface
 		, light_direction
@@ -545,5 +605,6 @@ vec3 computeLighting(Cluster cluster, Surface surface, vec3 light_direction, vec
 	res += surface.emission * surface.albedo;
 	res += pointLightsLighting(cluster, surface, shadow_atlas);
 	res += envProbesLighting(cluster, surface);
+	res += reflProbesLighting(cluster, surface, reflection_probes);
 	return res;
 }
